9e84880fd646613d477be26ea6163ccaaf25de7e
zezeze97 <18428398691@163.com>
2023-03-17 11:03:31 +0800

    update

---
+++ mmsegmentation-master/post_eval.py
@@ -0,0 +1,37 @@
+import h5py
+from monai.metrics import DiceMetric
+import os
+import torch
+import numpy as np
+from tqdm import tqdm
+import cv2
+
+dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False)
+
+def post_eval(predict_path, gt_path):
+    dice_scores = []
+    for i in range(20):
+        gts = np.zeros((128, 128, 128))
+        for k in range(128):
+            gt = cv2.imread(os.path.join(gt_path, f'cube_{i}_slice_{k}.png'), cv2.IMREAD_UNCHANGED)
+            gts[:,:,k] = gt
+        preds = np.zeros((128, 128, 128))
+        for k in range(128):
+            pred = cv2.imread(os.path.join(predict_path, f'cube_{i}_slice_{k}.png'), cv2.IMREAD_UNCHANGED)
+            preds[:,:,k] = pred
+        preds = torch.from_numpy(preds)
+        gts = torch.from_numpy(gts)
+        dice_metric(y_pred=preds, y=gts)
+        dice = dice_metric.aggregate().item()
+        dice_scores.append(dice)
+    print(f'Dice Score is: {np.mean(dice_scores)}')
+        
+            
+            
+            
+if __name__ == '__main__':
+    predict_path = '/home/zhangzr/FaultRecongnition/mmsegmentation-master/test_results/upernet_swin_base_patch4_window7_128x128_160k_fault_imagenet_pretrain_224x224_22K'
+    gt_path = '/home/zhangzr/FaultRecongnition/Fault_data/2d-simulate-data/val/ann'
+    post_eval(predict_path, gt_path)
+    
+    
--- mmsegmentation-master/run.sh
+++ mmsegmentation-master/run.sh
@@ -8,6 +8,6 @@ config=upernet_swin_base_patch4_window7_128x128_less_aug
 if [ $1 = "train" ]; then
     CUDA_VISIBLE_DEVICES=$GPU PORT=${port} ./tools/dist_train.sh configs/swin/${config}.py 1 --work-dir cache/${config} 
 elif [ $1 = "test" ]; then
-    CUDA_VISIBLE_DEVICES=$GPU ./tools/dist_test.sh configs/swin/${config}.py ./cache/${config}/best_mDice_epoch_18.pth 1 --format-only --work-dir cache/${config} --eval-options "imgfile_prefix=./test_results/${config}"
+    CUDA_VISIBLE_DEVICES=$GPU ./tools/dist_test.sh configs/swin/${config}.py ./cache/${config}/best_mDice_epoch_20.pth 1 --format-only --work-dir cache/${config} --eval-options "imgfile_prefix=./test_results/${config}"
 fi
 
