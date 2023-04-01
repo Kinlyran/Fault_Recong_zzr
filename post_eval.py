08e16209546518b45d3f614f011538910697a994
zezeze97 <18428398691@163.com>
2023-04-01 12:37:18 +0800

    update

--- post_eval.py
+++ post_eval.py
@@ -5,7 +5,13 @@ import torch
 import numpy as np
 from tqdm import tqdm
 
-dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False)
+
+def dice_coefficient(y_true, y_pred):
+    smooth = 1e-6
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
 
 def post_eval(predict_path, gt_path):
     pred_lst = os.listdir(predict_path)
@@ -15,10 +21,7 @@ def post_eval(predict_path, gt_path):
             pred = f['predictions'][:]
         with h5py.File(os.path.join(gt_path, item), 'r') as f:
             gt = f['label'][:]
-        pred = torch.FloatTensor(pred).unsqueeze(0)
-        gt = torch.FloatTensor(gt).unsqueeze(0)
-        dice_metric(y_pred=pred, y=gt)
-        dice = dice_metric.aggregate().item()
+        dice = dice_coefficient(gt, pred)
         dice_scores.append(dice)
     print(f'Dice Score is: {np.mean(dice_scores)}')
         
@@ -26,7 +29,7 @@ def post_eval(predict_path, gt_path):
             
             
 if __name__ == '__main__':
-    predict_path = '/home/zhangzr/FaultRecongnition/MIM-Med3D/output/Fault_Baseline/unetr_base_supbaseline_p16_fault/preds'
+    predict_path = '/home/zhangzr/FaultRecongnition/pytorch3dunet/3dunet-real/predict'
     gt_path = '/home/zhangzr/FaultRecongnition/Fault_data/real_labeled_data/crop/val'
     post_eval(predict_path, gt_path)
     
