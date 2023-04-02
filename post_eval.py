4af364646427aeaf6a698a2574d405ce201b3da5
zezeze97 <18428398691@163.com>
2023-04-02 18:06:20 +0800

    update

--- post_eval.py
+++ post_eval.py
@@ -1,7 +1,5 @@
 import h5py
-from monai.metrics import DiceMetric
 import os
-import torch
 import numpy as np
 from tqdm import tqdm
 
@@ -13,23 +11,35 @@ def dice_coefficient(y_true, y_pred):
     intersection = np.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
 
+def compute_acc(y_true, y_pred):
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    return np.sum(y_true_f==y_pred_f) / y_true_f.shape
+
+
 def post_eval(predict_path, gt_path):
     pred_lst = os.listdir(predict_path)
     dice_scores = []
+    acc_scores = []
     for item in pred_lst:
         with h5py.File(os.path.join(predict_path, item), 'r') as f:
             pred = f['predictions'][:]
         with h5py.File(os.path.join(gt_path, item), 'r') as f:
             gt = f['label'][:]
+
         dice = dice_coefficient(gt, pred)
         dice_scores.append(dice)
-    print(f'Dice Score is: {np.mean(dice_scores)}')
+
+        acc = compute_acc(gt, pred)
+        acc_scores.append(acc)
+    print(f'Dice Score is: {np.mean(dice_scores)} \n Acc is {np.mean(acc_scores)}')
+
         
             
             
             
 if __name__ == '__main__':
-    predict_path = '/home/zhangzr/FaultRecongnition/MIM-Med3D/output/Fault_Finetune/unetr_base_vitmae_p16_m0.75_fault_random_aug/preds'
+    predict_path = '/home/zhangzr/FaultRecongnition/MIM-Med3D/output/Fault_Baseline/unetr_base_supbaseline_p16_fault_random_aug/preds'
     gt_path = '/home/zhangzr/FaultRecongnition/Fault_data/real_labeled_data/crop/val'
     post_eval(predict_path, gt_path)
     
--- preprocess_raw_data.py
+++ preprocess_raw_data.py
@@ -3,17 +3,18 @@ import segyio
 import numpy as np
 import os
 from tqdm import tqdm
+import random
 
 
 
 def dat2h5():
-    data_path = '/home/zhangzr/FaultRecongnition/Fault_data/real_data'
-    seis_data = segyio.tools.cube(os.path.join(data_path, 'seis.sgy'))
+    data_path = '/home/zhangzr/FaultRecongnition/Fault_data/real_labeled_data'
+    seis_data = segyio.tools.cube(os.path.join(data_path, 'mig_fill.sgy'))
     # precess missing value
-    # seis_data[seis_data==-912300] = seis_data[seis_data!=-912300].mean()
-    seis_data[seis_data==0.0] = seis_data[seis_data!=0.0].mean()
-    # label = segyio.tools.cube(os.path.join(data_path, 'label_fill.sgy'))
-    # label.astype(np.uint8)
+    seis_data[seis_data==-912300] = seis_data[seis_data!=-912300].mean()
+    # seis_data[seis_data==0.0] = seis_data[seis_data!=0.0].mean()
+    label = segyio.tools.cube(os.path.join(data_path, 'label_fill.sgy'))
+    label.astype(np.uint8)
     print(f'min value is {seis_data.min()}, max value is {seis_data.max()}')
 
     slice_builder = SliceBuilder(raw_dataset=seis_data,
@@ -22,29 +23,30 @@ def dat2h5():
                                  patch_shape=(128, 128, 128),
                                  stride_shape=(128, 128, 128))
     crop_cubes_pos = slice_builder.raw_slices
-    train_save_path = '/home/zhangzr/FaultRecongnition/Fault_data/real_data/crop/train'
-    val_save_path = '/home/zhangzr/FaultRecongnition/Fault_data/real_data/crop/val'
+    train_save_path = '/home/zhangzr/FaultRecongnition/Fault_data/real_labeled_data/crop/train'
+    val_save_path = '/home/zhangzr/FaultRecongnition/Fault_data/real_labeled_data/crop/val'
     if not os.path.exists(train_save_path):
         os.makedirs(train_save_path)
     if not os.path.exists(val_save_path):
         os.makedirs(val_save_path)
     split_ratio = 0.9
+    train_split = random.sample([i for i in range(len(crop_cubes_pos))], int(split_ratio * len(crop_cubes_pos)))
     for i, pos in enumerate(crop_cubes_pos):
         x_range = pos[0]
         y_range = pos[1]
         z_range = pos[2]
         print(f'Processing num {i} slice')
         seis_cube_crop = seis_data[x_range, y_range, z_range]
-        # label_cube_crop = label[x_range, y_range, z_range]
-        if i < split_ratio * len(crop_cubes_pos):
+        label_cube_crop = label[x_range, y_range, z_range]
+        if i in train_split:
             f = h5py.File(os.path.join(train_save_path, f'{i}.h5'),'w') 
             f['raw'] = seis_cube_crop
-            # f['label'] = label_cube_crop
+            f['label'] = label_cube_crop
             f.close() 
         else:
             f = h5py.File(os.path.join(val_save_path, f'{i}.h5'),'w') 
             f['raw'] = seis_cube_crop
-            # f['label'] = label_cube_crop
+            f['label'] = label_cube_crop
             f.close() 
 
 class SliceBuilder:
