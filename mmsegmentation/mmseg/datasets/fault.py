acce900d4f42d71efe8fd8e347170d8b52ba0980
zezeze97 <18428398691@163.com>
2023-04-19 14:36:53 +0800

    update

--- mmsegmentation/mmseg/datasets/fault.py
+++ mmsegmentation/mmseg/datasets/fault.py
@@ -13,10 +13,8 @@ class FaultDataset(BaseSegDataset):
     def __init__(self,
                  img_suffix='.png',
                  seg_map_suffix='.png',
-                 reduce_zero_label=False,
                  **kwargs) -> None:
         super().__init__(
             img_suffix=img_suffix,
             seg_map_suffix=seg_map_suffix,
-            reduce_zero_label=reduce_zero_label,
             **kwargs)
