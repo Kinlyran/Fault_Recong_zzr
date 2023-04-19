51110a80db11e619dca14a78169ca09de4ea6764
zezeze97 <18428398691@163.com>
2023-04-19 19:33:19 +0800

    update

--- mmsegmentation/projects/Fault_recong/data_preprocess.py
+++ mmsegmentation/projects/Fault_recong/data_preprocess.py
@@ -5,26 +5,23 @@ import cv2
 import random
 
 def main(root_dir, save_dir):
-    feature_lst = glob.glob(root_dir + '*_Feature*.bin')
-    label_lst = glob.glob(root_dir + 'Label*.bin')
-    assert len(feature_lst) == len(label_lst)
     # create missing dir
     if not os.path.exists(save_dir):
         os.makedirs(os.path.join(save_dir, "image"))
         os.makedirs(os.path.join(save_dir, "ann"))
         
-    for idx in range(len(feature_lst)):
-        feature = np.fromfile(feature_lst[idx], dtype=np.double).reshape(128, 128)
+    for idx in range(172):
+        feature = np.fromfile(os.path.join(root_dir,f'GYX3D2018-PSDM-VTI-CG1203-400Km2-DP-50_Feature{str(idx)}.bin'), dtype=np.double).reshape(128, 128)
         feature = (feature - feature.min()) / (feature.max() - feature.min())
         feature = (255 * feature).astype(np.uint8)
-        label = np.fromfile(label_lst[idx], dtype=np.double).reshape(128, 128).astype(np.uint8)
+        label = np.fromfile(os.path.join(root_dir, f'Label{str(idx)}.bin'), dtype=np.double).reshape(128, 128).astype(np.uint8)
         # save
         feature_save_path = os.path.join(save_dir, "image", str(idx) + '.png')
         label_save_path = os.path.join(save_dir, "ann", str(idx) + '.png')
         cv2.imwrite(feature_save_path, feature)
         cv2.imwrite(label_save_path, label)
     # split data
-    all_data = [str(i) for i in range(len(feature_lst))]
+    all_data = [str(i) for i in range(172)]
     ratio = 0.9
     train_lst = random.sample(all_data, int(ratio * len(all_data)))
     val_lst = [item for item in all_data if item not in train_lst]
