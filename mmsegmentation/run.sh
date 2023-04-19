aeec49b148bcecd6008d271db64abfc1918c49df
zezeze97 <18428398691@163.com>
2023-04-19 14:25:13 +0800

    update

--- mmsegmentation/run.sh
+++ mmsegmentation/run.sh
@@ -7,7 +7,7 @@ port=23466
 config=swin-base-patch4-window7_upernet_8xb2-160k_fault-512x512
 
 if [ $1 = "train" ]; then
-    CUDA_VISIBLE_DEVICES=$GPU PORT=${port} ./tools/dist_train.sh configs/swin/${config}.py 1 --work-dir output/${config} 
+    CUDA_VISIBLE_DEVICES=$GPU PORT=${port} ./tools/dist_train.sh ./projects/Fault_recong/config/${config}.py 1 --work-dir output/${config} 
 elif [ $1 = "test" ]; then
     CUDA_VISIBLE_DEVICES=$GPU ./tools/dist_test.sh configs/swin/${config}.py ./cache/${config}/best_mDice_epoch_20.pth 1 --format-only --work-dir cache/${config} --eval-options "imgfile_prefix=./test_results/${config}"
 fi
