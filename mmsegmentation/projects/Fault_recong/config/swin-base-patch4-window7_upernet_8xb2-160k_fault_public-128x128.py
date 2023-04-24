497cc9550f6cae12fc5196fa8d20b8e9767ff732
zezeze97 <18428398691@163.com>
2023-04-24 22:08:08 +0800

    update

--- mmsegmentation/projects/Fault_recong/config/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128.py
+++ mmsegmentation/projects/Fault_recong/config/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128.py
@@ -104,7 +104,7 @@ tta_pipeline = [
 ]
 """
 train_dataloader = dict(
-    batch_size=8,
+    batch_size=16,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
@@ -115,7 +115,7 @@ train_dataloader = dict(
             img_path='train/image', seg_map_path='train/ann'),
         pipeline=train_pipeline))
 val_dataloader = dict(
-    batch_size=1,
+    batch_size=16,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
