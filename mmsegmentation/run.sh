b22678ef1fd268d564596d4ced24c9f9e1f5c109
zezeze97 <18428398691@163.com>
2023-04-26 13:43:34 +0800

    update

--- mmsegmentation/run.sh
+++ mmsegmentation/run.sh
@@ -1,13 +1,13 @@
 GPU=$2
-port=23466
+port=23467
 
 
 # config=swin-base-patch4-window7_upernet_8xb2-160k_fault-512x512
-config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public-512x512
+# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public-512x512
 # config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128
 
 if [ $1 = "train" ]; then
     CUDA_VISIBLE_DEVICES=$GPU PORT=${port} ./tools/dist_train.sh ./projects/Fault_recong/config/${config}.py 1 --work-dir output/${config} 
 elif [ $1 = "test" ]; then
-    CUDA_VISIBLE_DEVICES=$GPU ./tools/dist_test.sh configs/swin/${config}.py ./cache/${config}/best_mDice_epoch_20.pth 1 --format-only --work-dir cache/${config} --eval-options "imgfile_prefix=./test_results/${config}"
+    CUDA_VISIBLE_DEVICES=$GPU ./tools/dist_test.sh ./output/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128.py ./output/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128/iter_48000.pth 1
 fi
