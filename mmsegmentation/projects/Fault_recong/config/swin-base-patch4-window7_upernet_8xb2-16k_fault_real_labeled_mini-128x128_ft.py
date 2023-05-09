bf54ed7004a0da03198cd19b5f10da24c421ebbc
zezeze97 <18428398691@163.com>
2023-05-09 18:01:00 +0800

    update

--- mmsegmentation/projects/Fault_recong/config/swin-base-patch4-window7_upernet_8xb2-16k_fault_real_labeled_mini-128x128_ft.py
+++ mmsegmentation/projects/Fault_recong/config/swin-base-patch4-window7_upernet_8xb2-16k_fault_real_labeled_mini-128x128_ft.py
@@ -158,13 +158,13 @@ optim_wrapper = dict(
 param_scheduler = [
     dict(
         type='LinearLR', start_factor=1e-06, by_epoch=False, begin=0,
-        end=1500),
+        end=150),
     dict(
         type='PolyLR',
         eta_min=0.0,
         power=1.0,
-        begin=1500,
-        end=160000,
+        begin=150,
+        end=16000,
         by_epoch=False)
 ]
 train_cfg = dict(
