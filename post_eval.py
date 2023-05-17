04c5de2fe896b95f6fcfd59d97f8f572a638341e
zezeze97 <18428398691@163.com>
2023-05-17 17:38:09 +0800

    update

--- post_eval.py
+++ post_eval.py
@@ -2,17 +2,20 @@ import h5py
 import os
 import numpy as np
 from tqdm import tqdm
-from sklearn.metrics import average_precision_score
+from sklearn.metrics import f1_score, precision_score, recall_score
 import cv2
 
 
+ths = [i / 100 for i in range(1, 100)]
+print(f'ths is {ths}')
 
 def convert_gt(gt):
     h, w = gt.shape
     h_s, w_s = int(h/3), int(w/3)
-    gt = gt.astype(np.float32)
     gt = cv2.resize(gt,(w_s,h_s), interpolation=cv2.INTER_NEAREST)
-    gt = gt.astype(np.float32)
+    # gt = cv2.resize(gt,(w_s,h_s))
+    # gt = gt > 0.5
+    gt = gt.astype(np.uint8)
     return gt
 
 def convert_score(score):
@@ -20,51 +23,40 @@ def convert_score(score):
     h_s, w_s = int(h/3), int(w/3)
     score = cv2.resize(score,(w_s,h_s))
     return score
-
-def dice_coefficient(y_true, y_pred):
-    smooth = 1e-6
-    y_true_f = y_true.flatten()
-    y_pred_f = y_pred.flatten()
-    intersection = np.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
-
-def compute_acc(y_true, y_pred):
-    y_true_f = y_true.flatten()
-    y_pred_f = y_pred.flatten()
-    return np.sum(y_true_f==y_pred_f) / y_true_f.shape
-
-def compute_ap(y_true, y_score):
-    y_true_f = y_true.flatten()
-    y_score_f = y_score.flatten()
-    return average_precision_score(y_true_f, y_score_f)
     
     
 
 
 def post_eval(predict_path, gt_path):
-    pred = np.load(os.path.join(predict_path, 'predict.npy'), mmap_mode='r')
+    # pred = np.load(os.path.join(predict_path, 'predict.npy'), mmap_mode='r')
     score = np.load(os.path.join(predict_path, 'score.npy'), mmap_mode='r')
     # f =  h5py.File(predict_path, 'r')
     # pred = f['predictions']
     # score = f['score']
     
     gt = np.load(gt_path, mmap_mode='r')
-    
-    running_dice = []
-    running_acc = []
-    running_ap = []
+    image_f1_mat = np.zeros((len(range(0, gt.shape[0], 5)), len(ths)))
+    image_precision_mat = np.zeros((len(range(0, gt.shape[0], 5)), len(ths)))
+    k = 0
     for i in tqdm(range(0, gt.shape[0], 5)):
-        gt_slice = convert_gt(gt[i,:,800:1300])
-        pred_slice = convert_gt(pred[i,:,800:1300])
-        score_slice = convert_score(score[i,:,800:1300])
-        dice = dice_coefficient(gt_slice, pred_slice)
-        acc = compute_acc(gt_slice, pred_slice)
-        ap = compute_ap(gt_slice, score_slice)
-        running_dice.append(dice)
-        running_acc.append(acc)
-        running_ap.append(ap)
-    # f.close()
-    print(f'Dice Score is: {np.mean(running_dice)} \n Acc is {np.mean(running_acc)} \n Ap is {np.mean(running_ap)}')
+        # gt_slice = convert_gt(gt[i,:,800:1300])
+        gt_slice = gt[i,:,800:1300]
+        # pred_slice = convert_gt(pred[i,:,800:1300])
+        # score_slice = convert_score(score[i,:,800:1300])
+        score_slice = score[i,:,800:1300]
+        y_true_f = gt_slice.flatten()
+        y_score_f = score_slice.flatten()
+        j = 0
+        for th in tqdm(ths):
+            y_pred_f = (y_score_f > th).astype(np.uint8)
+            image_precision_mat[k, j] = precision_score(y_true_f, y_pred_f)
+            image_f1_mat[k, j] = f1_score(y_true_f, y_pred_f)
+            j += 1
+        k += 1
+    ap = np.mean(image_precision_mat)
+    OIS = np.mean(np.amax(image_f1_mat, axis=1))
+    ODS = np.amax(np.mean(image_f1_mat, axis=0))
+    print(f'Ap is {ap} \n OIS is {OIS}\n ODS is {ODS}')
         
             
             
