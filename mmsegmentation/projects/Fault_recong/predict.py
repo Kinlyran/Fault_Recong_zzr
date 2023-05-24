f123f3ba9f44b4cdd9f25c168c7b77166b648ace
zezeze97 <18428308691@163.com>
2023-05-24 17:00:53 +0800

    update

--- mmsegmentation/projects/Fault_recong/predict.py
+++ mmsegmentation/projects/Fault_recong/predict.py
@@ -50,6 +50,30 @@ def main(config_file, checkpoint_file, input_cube, save_path, device='cuda', for
         os.makedirs(save_path)
     np.save(os.path.join(save_path, 'predict.npy'), predict)
     np.save(os.path.join(save_path, 'score.npy'), prob)
+
+
+def main_2d(config_file, checkpoint_file, input_path, save_path, device='cuda', force_3_chan=False):
+    
+    # init model
+    model = init_model(config_file, checkpoint_file, device)
+    if device == 'cpu':
+        model = revert_sync_batchnorm(model)
+    
+    # get predice image list
+    pred_img_name_lst = os.listdir(input_path)
+    
+    # create missing dir
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+        
+        
+    print('start predict')
+    for img_name in tqdm(pred_img_name_lst):
+        image_slice = np.load(os.path.join(input_path, img_name))
+        if force_3_chan:
+            image_slice = np.stack([image_slice, image_slice, image_slice], axis=2)
+        result = inference_model(model, image_slice.copy())
+        np.save(os.path.join(save_path, img_name),torch.sigmoid(result.seg_logits.data.detach().cpu().squeeze(0)).numpy())
     
         
     
@@ -57,8 +81,15 @@ def main(config_file, checkpoint_file, input_cube, save_path, device='cuda', for
      
  
 if __name__ == '__main__':
-    config_file = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft_1/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft_1.py'
-    checkpoint_file = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft_1/Best_Dice_33.pth'
+    config_file = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft_3/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft.py'
+    checkpoint_file = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft_3/Best_Dice_28.pth'
     input_cube = segyio.tools.cube('/home/zhangzr/Fault_Recong/Fault_data/real_labeled_data/origin_data/seis/mig_fill.sgy')[373:,:,:]
-    save_path = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft_1/predict'
-    main(config_file, checkpoint_file, input_cube, save_path, device='cuda:0', convert_25d=True, step=5)
+    save_path = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft_3/predict'
+    main(config_file, checkpoint_file, input_cube, save_path, device='cuda:1', convert_25d=True, step=5)
+    
+    
+    # config_file = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_slice_split_force3chan-256x256/swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_slice_split_force3chan-256x256.py'
+    # checkpoint_file = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_slice_split_force3chan-256x256/Best.pth'
+    # input_path = '/home/zhangzr/Fault_Recong/Fault_data/2Dfault_0519_256/converted_slice_split/val/image'
+    # save_path = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_slice_split_force3chan-256x256/predict'
+    # main_2d(config_file, checkpoint_file, input_path, save_path, device='cuda:1', force_3_chan=True)
--- post_eval.py
+++ post_eval.py
@@ -4,6 +4,7 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.metrics import f1_score, precision_score, recall_score
 import cv2
+import segyio
 
 
 ths = [i / 100 for i in range(1, 100)]
@@ -27,7 +28,7 @@ def convert_score(score):
     
 
 
-def post_eval(predict_path, gt_path):
+def post_eval_thebe(predict_path, gt_path):
     # pred = np.load(os.path.join(predict_path, 'predict.npy'), mmap_mode='r')
     score = np.load(predict_path, mmap_mode='r')
     # f =  h5py.File(predict_path, 'r')
@@ -71,14 +72,68 @@ def post_eval_val(predict_path, gt_path):
         running_score.append(f1)
     print(f'Average F1 is {np.mean(running_score)}')
             
-            
+def post_eval_2d_image(predict_path, gt_path):
+    pred_image_name_lst = os.listdir(gt_path)
+    image_f1_mat = np.zeros((len(pred_image_name_lst), len(ths)))
+    image_precision_mat = np.zeros((len(pred_image_name_lst), len(ths)))
+    k = 0
+    for img_name in tqdm(pred_image_name_lst):
+        gt_slice = cv2.imread(os.path.join(gt_path, img_name), cv2.IMREAD_UNCHANGED)
+        score_slice = np.load(os.path.join(predict_path, img_name.replace('.png', '.npy')))
+        y_true_f = gt_slice.flatten()
+        y_score_f = score_slice.flatten()
+        j = 0
+        for th in ths:
+            y_pred_f = (y_score_f > th).astype(np.uint8)
+            image_precision_mat[k, j] = precision_score(y_true_f, y_pred_f)
+            image_f1_mat[k, j] = f1_score(y_true_f, y_pred_f)
+            j += 1
+        k += 1
+    ap = np.mean(image_precision_mat)
+    OIS = np.mean(np.amax(image_f1_mat, axis=1))
+    ODS = np.amax(np.mean(image_f1_mat, axis=0))
+    print(f'Ap is {ap} \n OIS is {OIS}\n ODS is {ODS}')
+
+
+
+def post_eval_3d(predict_path, gt_path):
+    # pred = np.load(os.path.join(predict_path, 'predict.npy'), mmap_mode='r')
+    score = np.load(predict_path, mmap_mode='r')
+    # f =  h5py.File(predict_path, 'r')
+    # pred = f['predictions']
+    # score = f['score']
+    
+    gt = segyio.tools.cube(gt_path)[373:,:,:]
+    image_f1_mat = np.zeros((gt.shape[0], len(ths)))
+    image_precision_mat = np.zeros((gt.shape[0], len(ths)))
+    k = 0
+    for i in tqdm(range(gt.shape[0])):
+        gt_slice = convert_gt(gt[i,:,:])
+        # gt_slice = gt[i,:,800:1300]
+        # pred_slice = convert_gt(pred[i,:,800:1300])
+        score_slice = convert_score(score[i,:,:])
+        # score_slice = score[i,:,800:1300]
+        y_true_f = gt_slice.flatten()
+        y_score_f = score_slice.flatten()
+        j = 0
+        for th in ths:
+            y_pred_f = (y_score_f > th).astype(np.uint8)
+            image_precision_mat[k, j] = precision_score(y_true_f, y_pred_f)
+            image_f1_mat[k, j] = f1_score(y_true_f, y_pred_f)
+            j += 1
+        k += 1
+    ap = np.mean(image_precision_mat)
+    OIS = np.mean(np.amax(image_f1_mat, axis=1))
+    ODS = np.amax(np.mean(image_f1_mat, axis=0))
+    print(f'Ap is {ap} \n OIS is {OIS}\n ODS is {ODS}')
             
 if __name__ == '__main__':
     # predict_path = '/home/zhangzr/FaultRecongnition/MIM-Med3D/output/Fault_Baseline/unetr_base_supbaseline_p16_public/test_pred/seistest.h5'
     # gt_path = '/home/zhangzr/FaultRecongnition/Fault_data/public_data/precessed/test/fault/faulttest.npy'
     # post_eval(predict_path, gt_path)
-    predict_path = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_public_slice-256x256/predict/score.npy'
-    gt_path = '/home/zhangzr/Fault_Recong/Fault_data/public_data/precessed/test/fault/faulttest.npy'
-    post_eval(predict_path, gt_path)
+    predict_path = '/home/zhangzr/Fault_Recong/mmsegmentation/output/swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256-dilate_ft_3/predict/score.npy'
+    gt_path = '/home/zhangzr/Fault_Recong/Fault_data/real_labeled_data/origin_data/fault/label_fill.sgy'
+    # post_eval(predict_path, gt_path)
     # post_eval_val(predict_path, gt_path)
+    post_eval_3d(predict_path, gt_path)
     
