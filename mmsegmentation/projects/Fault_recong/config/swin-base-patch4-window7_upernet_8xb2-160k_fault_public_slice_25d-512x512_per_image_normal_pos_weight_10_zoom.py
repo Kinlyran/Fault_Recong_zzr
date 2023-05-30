d57db977de9862d6fa9d8b951200d6df8c11b0f2
zezeze97 <18428398691@163.com>
2023-05-30 11:15:03 +0800

    update

--- mmsegmentation/projects/Fault_recong/config/swin-base-patch4-window7_upernet_8xb2-160k_fault_public_slice_25d-512x512_per_image_normal_pos_weight_10_zoom.py
+++ mmsegmentation/projects/Fault_recong/config/swin-base-patch4-window7_upernet_8xb2-160k_fault_public_slice_25d-512x512_per_image_normal_pos_weight_10_zoom.py
@@ -170,7 +170,7 @@ param_scheduler = [
         by_epoch=False)
 ]
 train_cfg = dict(
-    type='IterBasedTrainLoop', max_iters=160000, val_interval=160)
+    type='IterBasedTrainLoop', max_iters=160000, val_interval=16000)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
