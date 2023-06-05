3120ea9837eba2e665a425e2241119fab0daa12a
2001110054 <2001110054@wm1-data02.hpc.pku.edu.cn>
2023-06-05 10:11:17 +0800

    update

--- crop_raw_data.py
+++ crop_raw_data.py
@@ -52,14 +52,14 @@ def dat2h5():
     print('loading seis train data')
     seis_train = np.load(os.path.join(data_path, 'precessed', 'train', 'seis', 'seistrain.npy'), mmap_mode='r')
     fault_train = np.load(os.path.join(data_path, 'precessed', 'train', 'fault', 'faulttrain.npy'), mmap_mode='r')
-    get_slice(seis=seis_train, fault=fault_train, save_path=os.path.join(data_path, 'crop_512', 'train'), patch_shape=(256, 256, 256), stride_shape=(128, 128, 128))
+    get_slice(seis=seis_train, fault=fault_train, save_path=os.path.join(data_path, 'crop_256', 'train'), patch_shape=(256, 256, 256), stride_shape=(128, 128, 128))
     del seis_train
     del fault_train
     
     print('loading seis val data')
     seis_val = np.load(os.path.join(data_path, 'precessed','val', 'seis', 'seisval.npy'), mmap_mode='r')
     fault_val = np.load(os.path.join(data_path, 'precessed', 'val', 'fault', 'faultval.npy'), mmap_mode='r')
-    get_slice(seis=seis_val, fault=fault_val, save_path=os.path.join(data_path, 'crop_192x576x576', 'val'), patch_shape=(200, 256, 256), stride_shape=(10, 128, 128))
+    get_slice(seis=seis_val, fault=fault_val, save_path=os.path.join(data_path, 'crop_256', 'val'), patch_shape=(200, 256, 256), stride_shape=(10, 128, 128))
     del seis_val
     del fault_val
     
