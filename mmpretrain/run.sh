00c7505da7b6b133595807ceb57eea9560ab5439
zezeze97 <18428308691@163.com>
2023-06-15 17:13:55 +0800

    update

--- mmpretrain/run.sh
+++ mmpretrain/run.sh
@@ -1,5 +1,5 @@
 GPU=$2
-port=23490
+port=23491
 
 
 # config=simmim_swin-base-w6_100e_512x512_public
@@ -9,10 +9,11 @@ port=23490
 # config=simmim_swin-base-w7_3000e_512x512_public_25d_per_image_norm
 # config=simmim_swin-base-w7_3000e_512x512_public_force_3_chan_per_image_norm
 # config=simmim_swin-base-w7_100e_512x512_mix_force_3_chan_per_image_norm
-config=mae_vit-base-p16_8xb512-amp-coslr-300e_mix_force_3_chan_per_image_norm_norm_pix_False
+# config=mae_vit-base-p16_8xb512-amp-coslr-300e_mix_force_3_chan_per_image_norm_norm_pix_False
+config=simmim_swin-base-w7_100e_512x512_mix_force_3_chan_per_image_norm
 
 if [ $1 = "train" ]; then
-    CUDA_VISIBLE_DEVICES=$GPU PORT=${port} ./tools/dist_train.sh ./projects/Fault_Recong/config/${config}.py 2 --work-dir output/${config} 
+    CUDA_VISIBLE_DEVICES=$GPU PORT=${port} ./tools/dist_train.sh ./projects/Fault_Recong/config/${config}.py 8 --work-dir output/${config} 
 elif [ $1 = "test" ]; then
     CUDA_VISIBLE_DEVICES=$GPU ./tools/dist_test.sh ./output/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128.py ./output/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128/iter_48000.pth 1
 fi
